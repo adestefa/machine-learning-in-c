db3ade7cd7ac2c9f212a0358670abd20ae814de7
Anthony DeStefano <anthony.destefano@gmail.com>
2024-01-27 01:21:07 -0500

    OR Gate functioning model

--- gates.c
+++ gates.c
@@ -9,7 +9,10 @@
    trying to predict some number based on input number
    https://www.youtube.com/watch?v=PGSba51aRYU&t=4797s
 
-   >Model a single neuron with a two input signals
+   >Create a single neuron with two input signals
+   >Use sigmoid as our action function to constrain unbound values
+   >Model a simple OR Gate
+
   
      Artifical Neuron
    [x2] -w1- | LF | AF | 
@@ -26,14 +29,14 @@
   3.  Weights:             how strong is this conn      w1,w2
   3.  Linear Function:     sum the inputs with weights  x1*w1 + x2*w2...
   4.  Activation Function: constrain unbound values     sigmoid function maps -infinity / +infinty to 0/1
-  5.  Output:              
+  5.  Output:                                           y      
 
   */
 
 
 
 
-
+// constrain unbound values between 0 and 1
 float sigmoidf(float x)
 {
     return 1.f / (1.f + expf(-x));
